7e1a247436244160219d2d20a6746ec02d75f76c
judy <lee965870@naver.com>
2020-06-01 04:56:13 +0900

    연정연정

--- app/src/main/java/com/example/naemandong_main/rabbit/fragment/rScene02.java
+++ app/src/main/java/com/example/naemandong_main/rabbit/fragment/rScene02.java
@@ -28,6 +28,7 @@ public class rScene02 extends Fragment {
     MediaPlayer mp1 = new MediaPlayer();
     MediaPlayer mp2 = new MediaPlayer();
     MediaPlayer mp3 = new MediaPlayer();
+    MediaPlayer recordmp = new MediaPlayer();
     private View view;
     private ImageView background, rabbit, lion, sloth, box, next;
     private TextView subtitles;
@@ -59,14 +60,25 @@ public class rScene02 extends Fragment {
             e.printStackTrace();
         }
 
+        if(((Setting_data) getContext().getApplicationContext()).isRecordPlay()){
+            String path = ((Setting_data) getContext().getApplicationContext()).getRecordone();
+            ((Setting_data) getContext().getApplicationContext()).removeRecordData();
+            try {
+                recordmp.setDataSource(path);
+                recordmp.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         int a = mp1.getDuration();
         int b = mp1.getDuration() + mp2.getDuration();
         int c = mp1.getDuration() + mp2.getDuration() + mp3.getDuration();
 
-//        if (getArguments() != null){
-//            sound = getArguments().getBoolean("sound");
-//            subtitle = getArguments().getBoolean("subtitle");
-//        }
+        if (getArguments() != null){
+            sound = getArguments().getBoolean("sound");
+            subtitle = getArguments().getBoolean("subtitle");
+        }
 
         Glide.with(this)
                 .load("http://49.50.174.179:9000/images/rabbit/2/2_back.jpg")
@@ -107,13 +119,20 @@ public class rScene02 extends Fragment {
         });
 
         subtitles.setText(subs[0]);
-        mp1.start();
+        if(((Setting_data) getContext().getApplicationContext()).isRecordPlay()){
+            recordmp.start();
+        }
+        else {
+            mp1.start();
+        }
         delayHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 // TODO
                 subtitles.setText(subs[1]);
-                mp2.start();
+                if(!((Setting_data) getContext().getApplicationContext()).isRecordPlay()){
+                    mp2.start();
+                }
             }
         }, a);
         delayHandler.postDelayed(new Runnable() {
@@ -121,8 +140,9 @@ public class rScene02 extends Fragment {
             public void run() {
                 // TODO
                 subtitles.setText(subs[2]);
-                mp3.start();
-                //na.startAnimation(n);
+                if(!((Setting_data) getContext().getApplicationContext()).isRecordPlay()){
+                    mp3.start();
+                }      //na.startAnimation(n);
             }
         }, b);
         delayHandler.postDelayed(new Runnable() {
